543290d2ca53559db9ed8def4c66f3d99debe8f8
Benjamin <bnreinecke0209@gmail.com>
2018-09-11 14:43:02 -0400

    Checkpoint React To-Do: Components: Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
 class App extends Component {
   render() {
     return (
-      <li> A todo will go here </li>
+      <div className="App">
+        <ul>
+          <ToDo />
+          <ToDo />
+        </ul>
+      </div>
     );
   }
 }
